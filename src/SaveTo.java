a987219ff04a39865b47ecef5102bf4858281b6c
OlexandrBorovik <100314860+OlexandrBorovik@users.noreply.github.com>
2022-07-03 08:42:52 +0300

    Update SaveTo.java

--- src/SaveTo.java
+++ src/SaveTo.java
@@ -1,6 +1,7 @@
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Target(value = ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SaveTo {
 
